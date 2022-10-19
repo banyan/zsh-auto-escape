5b276a4938facad26567c5dbfd6e86758e4020eb
Kohei Hasegawa <ameutau@gmail.com>
2022-10-19 19:35:57 +0900

    Handle unintended match

--- zsh-dollar-sign-quoter.zsh
+++ zsh-dollar-sign-quoter.zsh
@@ -1,16 +1,26 @@
 declare -ga ZSH_DOLLAR_SIGN_QUOTER_PREFIXES
 
 function autoquote() {
+    local prefix_length command args
+
     for prefix in $ZSH_DOLLAR_SIGN_QUOTER_PREFIXES; do
-        for str ($prefix) {
-            if [[ ${(M)BUFFER#$str} ]] {
-                for F in ${(ps: :)BUFFER}; do
-                    if [[ $F =~ "\\$" ]] && ! [[ $F =~ "\\\\" ]] && ! [[ $F =~ "'" ]]; then
-                        BUFFER=${BUFFER/${F}/${(qq)${F}}}
-                    fi
-                done
-            }
-        }
+        # Assume that a prefix is always followed by a space
+        prefix_length=$(expr ${#prefix} + 1)
+        command=${BUFFER:0:$prefix_length}
+        args=${BUFFER:$prefix_length}
+
+        if [[ $command == "$prefix " ]]; then
+            for word in ${(ps: :)args}; do
+                # 1. if the word contains dollar sign
+                # 2. and not escaped like \$
+                # 3. and does not contain single quote
+                # 4. and does not contain double quote
+                if [[ $word =~ "\\$" ]] && ! [[ $word =~ "\\\\" ]] && ! [[ $word =~ "'" ]] && ! [[ $word =~ "\"" ]]; then
+                    # Enclose in single quote
+                    BUFFER=${BUFFER/${word}/${(qq)${word}}}
+                fi
+            done
+        fi
     done
     zle .accept-line
 }
